41222bd55ffa503bad92d658350cb011bfd398a9
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-07-13 22:33:15 +0530

    generated apis for chat gpt & dalle

--- lib/open_ai_service.dart
+++ lib/open_ai_service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:voic_assistant/secrets.dart';
 class OpenAiService {
+  final List<Map<String, String>> messages = [];
   Future<String> isArtPromptAPI(String prompt) async{
     try{
       final res = await http.post(
@@ -48,10 +49,72 @@ class OpenAiService {
   }
 
   Future<String> chatGPTAPI(String prompt) async{
-    return 'CHATGPT';
+      messages.add({
+        'role': 'user',
+        'content': prompt // Need to store for future prompts reference
+      });
+      try{
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openAIKey'
+        },
+        body: jsonEncode({
+          "model": "gpt-3.5-turbo",
+          "messages": messages,
+        })
+      );
+      print(res.body);
+      if(res.statusCode == 200){
+        String content = jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim(); //for spaces removal
+
+        messages.add({
+          'role': 'assistant',
+          "content": content,
+        });
+        return content;
+      }
+      return 'An Internal Error Occured!';
+    }
+    catch (e){
+      return e.toString();
+    }
   }
 
   Future<String> dallEApi(String prompt)async{
-    return 'DALL-E';
+        messages.add({
+        'role': 'user',
+        'content': prompt // Need to store for future prompts reference
+      });
+      try{
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/images/generations'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openAIKey'
+        },
+        body: jsonEncode({
+          'prompt': prompt, //generating only 1 image at a time
+          'n': 1,
+        })
+      );
+      print(res.body);
+      if(res.statusCode == 200){
+        String imageUrl = jsonDecode(res.body)['data'][0]['url'];
+        imageUrl = imageUrl.trim(); //for spaces removal
+
+        messages.add({
+          'role': 'assistant',
+          "content": imageUrl,
+        });
+        return imageUrl;
+      }
+      return 'An Internal Error Occured!';
+    }
+    catch (e){
+      return e.toString();
+    }
   }
 }
